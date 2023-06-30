b16048cc821b9fa7fe8b75761dd663d0ab22d825
Robin Long <binlon@amazon.com>
2023-06-30 16:16:16 +0800

    update ddp

---
+++ utils/r_push_to_hub.py
@@ -0,0 +1,30 @@
+import argparse
+
+def parse_parameters():
+    parser = argparse.ArgumentParser()
+
+    # Push to Hub Parameters
+    parser.add_argument("--push_to_hub", type=bool, default=True)
+    parser.add_argument("--hub_model_id", type=str, default=None)
+    parser.add_argument("--hub_strategy", type=str, default=None)
+    parser.add_argument("--hub_token", type=str, default=None)    
+
+    args, _ = parser.parse_known_args()
+
+    # make sure we have required parameters to push
+    if args.push_to_hub:
+        if args.hub_strategy is None:
+            raise ValueError("--hub_strategy is required when pushing to Hub")
+        if args.hub_token is None:
+            raise ValueError("--hub_token is required when pushing to Hub")
+
+    # sets hub id if not provided
+    if args.hub_model_id is None:
+        args.hub_model_id = args.model_id.replace("/", "--")  
+
+    return args
+
+def push_to_hub(trainer, model_id):
+    # save best model, metrics and create model card
+    trainer.create_model_card(model_name=model_id)
+    trainer.push_to_hub()
